81a3b31b80ff5ff061c8a53f07f21a0a3085227f
ThankYouCode <gauma437@gmail.com>
2023-03-24 13:01:30 -0700

    Added app.run code in app.py

--- app.py
+++ app.py
@@ -11,4 +11,7 @@ def index():
 @app.route("/greet", methods=["POST", "GET"])
 def greet():
     flash("Hi " + str(request.form['name_input']) + ", great to see you!")
-    return render_template("index.html")    
+    return render_template("index.html")   
+
+if __name__ == "__main__":
+    app.run(debug=False,host='0.0.0.0')
